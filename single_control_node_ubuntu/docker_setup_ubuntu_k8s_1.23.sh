ac30076b81250f47adbea3a76e3615d1ac3084c0
brianandr <103252279+brianandr@users.noreply.github.com>
2022-04-22 18:44:06 +0300

    create:docker runtime installation file

---
+++ single_control_node_ubuntu/docker_setup_ubuntu_k8s_1.23.sh
@@ -0,0 +1,22 @@
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Install using the repository
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# set the cgroup to systemd requred by kubeadm by default as of Apri 2022 on ubuntu systems
+echo '{ "exec-opts": ["native.cgroupdriver=systemd"] }' > /etc/docker/daemon.json
