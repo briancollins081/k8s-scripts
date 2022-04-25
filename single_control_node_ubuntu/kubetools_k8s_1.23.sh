ec7648051a0e622108bc35bcf5560423b8b86bb8
Brian A. Collins <33161690+briancollins081@users.noreply.github.com>
2022-04-25 22:55:25 +0300

    Update kubetools_k8s_1.23.sh

--- single_control_node_ubuntu/kubetools_k8s_1.23.sh
+++ single_control_node_ubuntu/kubetools_k8s_1.23.sh
@@ -14,18 +14,10 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-sysctl -w net.ipv4.ip_forward=1
-
 # Install kubectl, kubeadm and kublet
-
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
